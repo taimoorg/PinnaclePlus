59caf55a0040ff1a35c243f9fa42218efc55ec2e
taimoor <taimoorali504@gmail.com>
2017-04-28 19:14:09 +0500

    getdoc

---
+++ PinnaclePlus/getdoc.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class getdoc
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        PinnaclePlusGlobals.GetDocument(Request.QueryString("ID"))
+    End Sub
+
+End Class
